bc7e504de2cc9c640b61221e8a89d594f777a67b
yhkim <kvoyant@gmail.com>
2019-05-14 15:50:42 +0900

    은행이름만 따서 로그찍기

--- app/src/main/java/com/tjeit/banklistfromserver02/MainActivity.java
+++ app/src/main/java/com/tjeit/banklistfromserver02/MainActivity.java
@@ -3,12 +3,14 @@ package com.tjeit.banklistfromserver02;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
 import com.tjeit.banklistfromserver02.databinding.ActivityMainBinding;
 import com.tjeit.banklistfromserver02.utils.ConnectServer;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -45,6 +47,22 @@ public class MainActivity extends BaseActivity {
                                 public void run() {
                                     if(code == 200 ) {
                                         Toast.makeText(mContext,"정상적으로 데이터를 가져왔습니다.", Toast.LENGTH_SHORT).show();
+
+//                                        은행이름만 따서 로그찍기
+                                        try {
+                                            JSONObject data = json.getJSONObject("data");
+                                            JSONArray banks = data.getJSONArray("banks");
+
+                                            for(int i=0;i<banks.length();i++) {
+                                                JSONObject bank = banks.getJSONObject(i);
+                                                String name = bank.getString("name");
+
+                                                Log.d("은행이름",name);
+                                            }
+
+                                        } catch (JSONException e) {
+                                            e.printStackTrace();
+                                        }
                                     }
                                     else {
 //                                        서버에서 주는 에러메세지를 토스트로 출력
