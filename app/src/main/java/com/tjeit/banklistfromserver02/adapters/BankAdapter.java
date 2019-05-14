a20866d243b2582afc99b955b0228ae7a7f8172c
yhkim <kvoyant@gmail.com>
2019-05-15 02:20:08 +0900

    어댑터 작성 완료

--- app/src/main/java/com/tjeit/banklistfromserver02/adapters/BankAdapter.java
+++ app/src/main/java/com/tjeit/banklistfromserver02/adapters/BankAdapter.java
@@ -2,7 +2,11 @@ package com.tjeit.banklistfromserver02.adapters;
 
 import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.tjeit.banklistfromserver02.R;
 import com.tjeit.banklistfromserver02.datas.Bank;
@@ -21,5 +25,26 @@ public class BankAdapter extends ArrayAdapter<Bank> {
         mContext = context;
         mList = list;
         inf = LayoutInflater.from(mContext);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        View row = convertView;
+
+        if(row == null) {
+            row = inf.inflate(R.layout.bank_list_item, null);
+        }
+
+        Bank bankData = mList.get(position);
+
+        ImageView logoImg = row.findViewById(R.id.logoImg);
+        TextView bankCodeTxt = row.findViewById(R.id.bankCodeTxt);
+        TextView bankNameTxt = row.findViewById(R.id.bankNameTxt);
+
+        bankNameTxt.setText(bankData.name);
+        bankCodeTxt.setText(String.format("(%s)", bankData.code));
+
+        return row;
     }
 }
