f51309f7c88555460b72ff939f23b929e6066331
yhkim <kvoyant@gmail.com>
2019-05-14 16:20:44 +0900

    은행 JSON -> 은행 클래스 얻어내기 까지

---
+++ app/src/main/java/com/tjeit/banklistfromserver02/datas/Bank.java
@@ -0,0 +1,28 @@
+package com.tjeit.banklistfromserver02.datas;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Bank {
+    public int id;
+    public String name;
+    public String code;
+    public String logo;
+
+    public static Bank getBankFromJson(JSONObject bankJson) {
+        Bank bankObject = new Bank();
+
+        try {
+
+            bankObject.id = bankJson.getInt("code");
+            bankObject.code = bankJson.getString("code");
+            bankObject.name = bankJson.getString("name");
+            bankObject.logo = bankJson.getString("logo");
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return bankObject;
+    }
+}
